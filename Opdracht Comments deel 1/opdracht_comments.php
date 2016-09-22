a3198db6112103a3e66a145efcabf308d5326f6e
Woozsh Laptop <jordy.pereira@hotmail.com>
2016-09-22 17:56:47 +0200

    Opdracht 1 + 2 + 3

---
+++ Opdracht Comments deel 1/opdracht_comments.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Opdracht Comments: deel 1</title>
+</head>
+<body>
+  <?php
+    $surname = "Jordy";
+    $name = "Pereira";
+    #Jordy Pereira jordy.pereira@student.kdg.be
+  ?>
+    <h1><?= $surname ?>  <?= $name ?></h1>
+</body>
+</html>
