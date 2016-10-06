ec0b9983886b50c99e678f93aa096c6c8414bece
Woozsh Laptop <jordy.pereira@hotmail.com>
2016-10-06 15:28:45 +0200

    opdracht if

--- opdracht-string-extra-functions/deel-1.php
+++ opdracht-string-extra-functions/deel-1.php
@@ -2,9 +2,16 @@
   $fruit = "kokosnoot";
   $fruitlen = strlen($fruit);
   $fruitposo = strpos($fruit, "o");
-  echo $fruitlen;
-  echo "\r\n";
-  echo $fruitposo;
-
-
  ?>
+ 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Opdracht Kokosnoot</title>
+  </head>
+  <body>
+    <p><?= $fruitlen ?> </p>
+    <p><?= $fruitposo ?> </p>
+  </body>
+</html>
---
+++ opdracht-string-extra-functions/deel-2.php
@@ -0,0 +1,16 @@
+<?php
+  $fruit = "ananas";
+  $fruita = strpos($fruit, "a");
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Opdracht Ananas</title>
+  </head>
+  <body>
+
+  </body>
+</html>
