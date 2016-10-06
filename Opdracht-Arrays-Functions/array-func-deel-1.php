e9407f36d53a3a6a468e8ffcded2ac3a87379d0b
Woozsh Laptop <jordy.pereira@hotmail.com>
2016-10-06 17:15:05 +0200

    dieren array

---
+++ Opdracht-Arrays-Functions/array-func-deel-1.php
@@ -0,0 +1,28 @@
+<?php
+$dieren = array("koe","kat","hond","schaap","varken");
+
+$dierencount = count($dieren);
+$teZoekenDier = "kat";
+$dierGevonden = in_array($teZoekenDier, $dieren);
+if($dierGevonden) $output = "Gevonden";
+else $output = "Niet Gevonden";
+ ?>
+
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>Dieren arrays</title>
+   </head>
+   <body>
+     <p>
+       Er zijn
+       <?= $dierencount ?>
+        verschillende dieren in de boerderij.
+     </p>
+     <p>
+       Het dier is <?= $output ?>
+     </p>
+   </body>
+ </html>
