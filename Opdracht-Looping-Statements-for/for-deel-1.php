8f879a05e718592a3b6bb9a2562b1d14b3b92fc0
Woozsh Laptop <jordy.pereira@hotmail.com>
2016-10-07 20:32:21 +0200

    opdracht for loop deel 1 & 2

---
+++ Opdracht-Looping-Statements-for/for-deel-1.php
@@ -0,0 +1,27 @@
+<?php
+$rijen = 10;
+$kolommen = 10;
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>For loop</title>
+  </head>
+  <body>
+    <table class="table table-bordered">
+      <?php for ($i=0; $i < $rijen; $i++) { ?>
+        <tr>
+          <?php for ($j=0; $j < $kolommen; $j++) { ?>
+            <td>
+              kolom
+            </td>
+          <?php } ?>
+        </tr>
+      <?php } ?>
+    </table>
+  </body>
+</html>
