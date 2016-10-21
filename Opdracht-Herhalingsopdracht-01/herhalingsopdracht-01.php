f977d8f36ba66e3817f361d10a409640f00164c7
Woozsh Laptop <jordy.pereira@hotmail.com>
2016-10-21 02:59:46 +0200

    Completed Herhalingsoef-01 with expansion

--- Opdracht-Herhalingsopdracht-01/herhalingsopdracht-01.php
+++ Opdracht-Herhalingsopdracht-01/herhalingsopdracht-01.php
@@ -1,10 +1,52 @@
 <?php
-  function searchFiles(){
-    if(isset($_GET[])){
-      
-    }
-  }
 
+$filesFound = false;
+if(isset($_GET['zoeken'])){
+  $text = $_GET['zoeken'];
+}else $text = "";
+
+  function searchFiles(){
+      $page = "../../cursus/public/cursus/voorbeelden/";
+      $pageOpd = "../opdrachten";
+      chdir($page);
+      $text = $_GET['zoeken'];
+      $arrayFiles = "";
+      $pagelink = "http://web-backend.local/cursus/voorbeelden/";
+      $pagelinkOpd = "http://web-backend.local/cursus/opdrachten/";
+      ?>
+      <h3>Zoeken op "<?= $text ?>"</h3>
+      <ul class="voorbeelden">
+        <h4>Voorbeelden</h4>
+      <?php foreach (glob("*/*.php") as $file):
+        if(strpos($file, $text) != false):
+          $arrayFiles = $file;?>
+
+          <li>
+            <a href=<?= $pagelink . $file ?> target="_blank">
+              <?= $file ?>
+            </a>
+          </li>
+      <?php endif; endforeach; chdir($pageOpd); ?>
+      <h4>Opdrachten</h4>
+      <?php foreach (glob("*/*.html") as $file):
+        if(strpos($file, $text) != false):
+          $arrayFiles = $file;?>
+
+          <li>
+            <a href=<?= $pagelinkOpd . $file ?> target="_blank">
+              <?= $file ?>
+            </a>
+          </li>
+      <?php endif; endforeach; ?>
+      </ul>
+      <?php
+      if($arrayFiles == ""): ?>
+        <h4>No files found</h4>
+        <?php
+      else:
+      $filesFound = true;
+      endif;
+    }
  ?>
 
 <!DOCTYPE html>
@@ -13,6 +55,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css" media="screen" title="no title">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Indexpagina</title>
   </head>
   <body>
@@ -26,7 +69,7 @@
     <form action="herhalingsopdracht-01.php" method="get">
       <div class="form-group">
         <label for="zoeken">Zoek naar: </label>
-        <input type="search" name="zoeken" placeholder="Geef een zoekterm in">
+        <input type="search" name="zoeken" placeholder="<?php if($text != ""): echo $_GET['zoeken']; else: echo "Geef een zoekterm in"; endif; ?>">
         <input type="submit" class="btn btn-primary" name="send" value="Search">
       </div>
     </form>
@@ -34,6 +77,10 @@
     <div class="inhoud">
       <?php
         $page = null;
+
+        if(isset($_GET['send']) && !empty($_GET['zoeken'])){
+          searchFiles();
+        }
         if(isset($_GET['link'])){
           $link = $_GET['link'];
           switch($link){
