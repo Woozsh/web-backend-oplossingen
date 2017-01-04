8d83c09c4c717f06258596d402700f88c916dc86
Woozsh <jordy.pereira@hotmail.com>
2017-01-04 01:57:59 +0100

    finished mod rewrite basis

---
+++ Opdracht-Mod-Rewrite-Basis/deel-2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>var dump</title>
+  </head>
+  <body>
+    <h1>var_dump() van $_GET variabele</h1>
+    <pre>
+      <code>
+        <?php
+        var_dump($_GET);
+         ?>
+      </code>
+    </pre>
+
+  </body>
+</html>
---
+++ Opdracht-Mod-Rewrite-Basis/deel-3/original.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Het Originele Bestand</title>
+  </head>
+  <body>
+    <h1>Het Originele Bestand</h1>
+  </body>
+</html>
